9e792dccca090a10da108bec9731395b41eae8d1
philliptaylormcgrath <philliptaylormcgrath@gmail.com>
2019-10-31 10:26:21 -0400

    THIS IS A MEANINGFULL MESSAGE

---
+++ src/co/grandcircus/MainApp.java
@@ -0,0 +1,11 @@
+package co.grandcircus;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		System.out.println("HELLO MATT");
+		System.out.println("MY NAME IS MOON MAN");
+
+	}
+
+}
